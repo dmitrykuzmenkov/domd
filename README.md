f14b6eed194913a87480c087a8e78628f6b0db78
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-11-20 16:13:47 +0700

    Initial release

---
+++ README.md
@@ -0,0 +1,45 @@
+DOMD
+======
+Tiny dependency free DOM delegator.
+
+## Installation
+
+Use npm package manager to install the DOM delegator
+```bash
+npm install domd
+```
+
+## Usage
+
+Follow the example:
+
+```html
+<button class="js-button">Click me</button>
+```
+
+```javascript
+var domd = require('domd');
+var d = domd(document.body);
+
+d.on('click', '.js-button', function(ev, el) {
+  console.log('Button clicked!');
+});
+
+```
+
+## Methods
+
+### on(event, selector, callback)
+
+Bind new listener to all elements.
+
+event - javascript event to catch (click, keyup and etc)
+selector - valid document query selector
+callback - function to callback. Arguments: event, element
+
+### off(event, selector)
+
+Disable listeners from all elements using selector.
+
+event - javascript event
+selector - valid document query selector
---
+++ index.js
@@ -0,0 +1,56 @@
+var event_target = function (elements, target) {
+  for (var i = 0, l = elements.length; i < l; i++) {
+    if (elements[i] === target) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+var event_handler = function(root, listeners) {
+  return function (e) {
+    var target = event.target;
+    if (target.nodeType === 3) {
+      target = target.parentNode;
+    }
+
+    for (var i in listeners) {
+      if (event_target(root.querySelectorAll(listeners[i].selector), target)) {
+        listeners[i].callback(e, target);
+        break;
+      }
+    }
+  };
+};
+
+module.exports = function(node) {
+  var listeners = [];
+  return {
+    on: function(event, selector, callback) {
+      var add_listener = false;
+
+      if (!listeners[event]) {
+        listeners[event] = [];
+        add_listener = true;
+      }
+
+      if (add_listener) {
+        node.addEventListener(event, event_handler(node, listeners[event]));
+      }
+
+      listeners[event].push({
+        selector: selector,
+        callback: callback
+      });
+    },
+
+    off: function(event, selector) {
+      for (var i in listeners) {
+        if (listeners[i].selector === selector) {
+          listeners.splice(i, 1);
+        }
+      }
+    }
+  };
+};
