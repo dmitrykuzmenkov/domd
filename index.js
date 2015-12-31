e5d16e968f283e253d17755de1b6c9445085a5eb
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-12-31 15:02:20 +0700

    Add event phase handling

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 var match_selector = require('./match_selector.js');
 
-var event_handler = function (root, listeners) {
+var event_handler = function (root) {
   return function (e) {
     var target = event.target;
+
     if (target.nodeType === 3) {
       target = target.parentNode;
     }
 
-    for (var i in listeners) {
-      if (match_selector(root, target, listeners[i].selector)) {
-        listeners[i].callback(e, target);
+    var list = [];
+    var phase = event.eventPhase || ( event.target !== event.currentTarget ? 3 : 2 );
+
+    switch (phase) {
+      case 1: //Event.CAPTURING_PHASE:
+        list = listeners[1][e.type];
+        break;
+
+      case 2: //Event.AT_TARGET:
+        if (listeners[0][e.type]) {
+          list = list.concat(listeners[0][e.type]);
+        }
+
+        if (listeners[1][e.type]) {
+          list = list.concat(listeners[1][e.type]);
+        }
+        break;
+
+      case 3: //Event.BUBBLING_PHASE:
+        list = listeners[0][e.type];
+      break;
+    }
+
+    for (var i in list) {
+      if (match_selector(root, target, list[i].selector)) {
+        list[i].callback(e, target);
         break;
       }
     }
@@ -26,13 +50,14 @@ module.exports = function (node) {
       var map = listeners[use_capture ? 1 : 0];
       if (!map[event]) {
         map[event] = [];
-        node.addEventListener(event, event_handler(node, map[event]), !!use_capture);
+        node.addEventListener(event, event_handler(node), !!use_capture);
       }
 
       map[event].push({
         selector: selector,
         callback: callback
       });
+      console.log(listeners);
     },
 
     off: function (event, selector) {
